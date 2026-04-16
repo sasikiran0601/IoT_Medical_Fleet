df11a767c9138fb15c9b6addad869d18e2310af0
sasikiran0601 <sasikiran0601@gmail.com>
2026-04-16 19:53:14 +0530

    Fix admin API calls to canonical slash routes

--- medical-iot-fleet/frontend/src/api/apikeys.js
+++ medical-iot-fleet/frontend/src/api/apikeys.js
@@ -1,6 +1,6 @@
 import api from "./axiosInstance";
 
-export const getApiKeys = () => api.get("/api/keys");
+export const getApiKeys = () => api.get("/api/keys/");
 export const regenerateKey = (deviceId) => api.post(`/api/keys/${deviceId}/regenerate`);
 export const getExampleCode = (deviceId) => api.get(`/api/keys/example/${deviceId}`);
 export const testWebhook = (deviceId) => api.post(`/api/webhooks/${deviceId}/test`);
--- medical-iot-fleet/frontend/src/api/userapi.js
+++ medical-iot-fleet/frontend/src/api/userapi.js
@@ -1,5 +1,5 @@
 import api from "./axiosInstance";
 
-export const getUsers = () => api.get("/api/users");
+export const getUsers = () => api.get("/api/users/");
 export const updateUser = (id, data) => api.put(`/api/users/${id}`, data);
 export const deleteUser = (id) => api.delete(`/api/users/${id}`);
